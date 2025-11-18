4a060f9f40353cbe2fcb08723b6e4460c9492166
ketigabunia15-hash <ketigabunia15@gmail.com>
2025-11-18 18:05:48 +0400

    switch კვირის დღეზე

--- main.js
+++ main.js
@@ -22,3 +22,30 @@ for (let k=0; k<numbers.length; k++) {
     }
 }
 
+const currentDate=new Date().getDay();
+switch (currentDate) {
+    case 0:
+        console.log("კვირა");
+        break;
+    case 1:
+        console.log("ორშაბათი");
+        break;
+    case 2:
+        console.log("სამშაბათი");
+        break;  
+    case 3:
+        console.log("ოთხშაბათი");
+        break;
+    case 4: 
+        console.log("ხუთშაბათი");
+        break;
+    case 5:
+        console.log("პარასკევი");
+        break;  
+    case 6:
+        console.log("შაბათი");
+        break;
+    default:
+        console.log("არასწორი დღე");
+        break;
+}
